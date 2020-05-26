d140682b1a2244c0eb898c3b2a86ec233b180723
Karen Karagebakyan <mkaragebakyan@gmail.com>
2020-05-26 13:41:41 +0400

    adding new methods to main_page.py change the exception

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,23 @@
 import time
+from selenium.common.exceptions import NoSuchElementException
 
-class BasePage():
+class BasePage(object):
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
         time.sleep(5)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
 
 
