4f729fb7ce9a043ccfa79b18515fb0ba6a75a135
Karen Karagebakyan <mkaragebakyan@gmail.com>
2020-05-27 14:50:21 +0400

    add methods to click 'add basket'

--- pages/locators.py
+++ pages/locators.py
@@ -3,10 +3,17 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BASKET_CLICK = (By.CSS_SELECTOR, "add_to_basket_form")
 
 
 class LoginPageLocators():
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+
+class BasketPageLocators():
+    NAME_PRODUCT = (By.CSS_SELECTOR, "#messages .alert:first-child strong")
+
+
 
 
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,19 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
 from .login_page import LoginPage
+from .locators import BasketPageLocators
 
 
 class MainPage(BasePage):
+
+    def expected_result(self):
+        name_product = self.browser.find_element_by_tag_name('h1').text
+        name_expected_result = self.browser.find_element(*BasketPageLocators.NAME_PRODUCT).text
+        assert name_expected_result == name_product, "do not match name"
+
+    def go_to_basket_page(self):
+        link1 = self.browser.find_element_by_id('add_to_basket_form')
+        link1.click()
 
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
